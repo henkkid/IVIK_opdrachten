6168bf02890daa87db482e1157aa71c886368671
henkkid <roelofjantimmerman@gmail.com>
2018-12-19 20:40:24 +0100

    optimesed code

--- assignment_2/complex_solution/app.py
+++ assignment_2/complex_solution/app.py
@@ -4,17 +4,16 @@ from pie import Pie
 from controller import Controller
 
 print_options = Print_options()
+list_of_pies = []
+list_of_pies.append(Pie("Chocolat delight", 10, 28))
+list_of_pies.append(Pie("Bitter sweet story of vanilla", 8, 20))
+list_of_pies.append(Pie("Black forrest extra vergance", 8, 18))
+
 
 number_of_persons = print_options.display_float_question("aantal personen? ")
 budget = print_options.display_float_question("wat is het budget? ")
 
 party = Party(number_of_persons, budget)
-
-list_of_pies = []
-
-list_of_pies.append(Pie("Chocolat delight", 10, 28))
-list_of_pies.append(Pie("Bitter sweet story of vanilla", 8, 20))
-list_of_pies.append(Pie("Black forrest extra vergance", 8, 18))
 
 controller = Controller(list_of_pies, party)
 
